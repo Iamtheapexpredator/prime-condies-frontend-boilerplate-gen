7eb9987e3e3f8d1a909a457a1d11bffb9dfbf640
Iamtheapexpredator <danielsonlukea@gmail.com>
2018-12-28 18:44:16 -0700

    went through some react stuff

--- src/App.js
+++ src/App.js
@@ -1,14 +1,87 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
+import Button from './components/Button';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+const isSearched = (searchTerm, item) => {
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: list,
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <h2>Welcome to the Road to learn React</h2>
+      <div className='App'>
+        {this.state.list.map(item => (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <Button onClick={() => this.onDismiss(item.objectID)}>
+              Dismiss
+            </Button>
+          </div>
+        ))}
       </div>
     );
   }
 }
+
+const Search = ({ value, onChange, children }) => {
+  return (
+    <form>
+      {children}
+      <input type='text' value={value} onChange={onChange} />
+    </form>
+  );
+};
+
+const Table = ({ list, pattern, onDismiss }) => {
+  return (
+    <div>
+      {list.filter(isSearched(pattern)).map(item => (
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+          <span>
+            <button onClick={() => onDismiss(item.objectID)}>Dismiss</button>
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
--- src/stories/index.js
+++ src/stories/index.js
@@ -3,11 +3,12 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import Button from '../components/Button';
 
-import { Button, Welcome } from '@storybook/react/demo';
+storiesOf('Button', module).add('Default', () => (
+  <Button onClick={() => console.log('click')} />
+));
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+// storiesOf('Button', module).add('with text', () => (
+//   <Button onClick={action('clicked')}>Hello Button</Button>
+// ));
